e64393d4a34d0250662dab4944b9ae913bd03417
Alessandro Felaco <alessandro.felaco.91@gmail.com>
2025-10-18 17:06:08 +0200

    Fix kv details changes

--- src/homeauto/core/dataset/bronze/__init__.py
+++ src/homeauto/core/dataset/bronze/__init__.py
@@ -1,12 +1,12 @@
 from dataclasses import dataclass
 
 from homeauto.core.dataset import Dataset
-from homeauto.core.infra import infra
+from homeauto.core.infra import StorageAccount, infra
 
 
 @dataclass
 class BronzeDataset(Dataset):
-    storage_account: str = infra.datalake.bronze
+    storage_account: StorageAccount = infra.datalake.bronze
 
     def get_path(self) -> str:
         return f"abfss://{self.container}/{self.name}.parquet"
