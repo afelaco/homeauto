1818e3302fcd6c8d1cc8e1fef0ba2710a97329f5
Alessandro Felaco <113336615+afelaco@users.noreply.github.com>
2025-10-18 13:36:10 +0200

    Terraform Output Vars (#7)
    * dev
    
    * dev

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,3 @@
 output "pypi_name" { value = module.pypi.name }
 output "pypi_container_name" { value = module.pypi.container_name }
-output "sa_bronze_name" { value = module.sa-bronze.sa_name }
-output "sa_silver_name" { value = module.sa-silver.sa_name }
-output "sa_gold_name" { value = module.sa-gold.sa_name }
+output "sa_name" { value = { for k, m in module.sa : k => m.sa_name } }
