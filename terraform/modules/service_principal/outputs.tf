b2744bb31e2d42a83f4dd74c27381d6dc1e741f7
Alessandro Felaco <alessandro.felaco.91@gmail.com>
2025-10-22 20:33:06 +0200

    dev

--- terraform/modules/service_principal/outputs.tf
+++ terraform/modules/service_principal/outputs.tf
@@ -1,8 +1,9 @@
 output "credentials" {
-  value = {
-    "subscriptionId" : var.tenant_id,
-    "tenantId" : var.subscription_id,
-    "clientId" : azuread_service_principal.this.client_id,
-    "clientSecret" : azuread_service_principal_password.this.value
-  }
+  value = jsonencode({
+    subscriptionId = var.tenant_id,
+    tenantId       = var.subscription_id,
+    clientId       = azuread_service_principal.this.client_id,
+    clientSecret   = azuread_service_principal_password.this.value
+  })
+  sensitive = true
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,32 @@
-output "pypi_storage_account" { value = module.pypi.name }
-output "pypi_container" { value = module.pypi.container_name }
-
-output "keyvault" {
-  value = {
-    uri = module.kv.uri
-  }
+output "pypi" {
+  value = jsonencode({
+    name      = module.pypi.name
+    container = module.pypi.container_name
+  })
 }
 
-output "datalake" {
-  value = {
-    for k, m in module.datalake : k => {
-      name   = m.storage_account_name
-      secret = m.secret_name
+output "infrastructure" {
+  value = jsonencode(
+    {
+      keyvault = {
+        uri = module.kv.uri
+      }
+      datalake = {
+        bronze = {
+          name   = module.datalake["bronze"].storage_account_name
+          secret = module.datalake["bronze"].secret_name
+        }
+        silver = {
+          name   = module.datalake["silver"].storage_account_name
+          secret = module.datalake["silver"].secret_name
+        }
+        gold = {
+          name   = module.datalake["gold"].storage_account_name
+          secret = module.datalake["gold"].secret_name
+        }
+      }
     }
-  }
+  )
 }
 
 output "airflow_sp_creds" {
