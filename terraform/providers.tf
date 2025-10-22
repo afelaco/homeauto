58c0b85553a27a96ddf0c082d767caa5175bc887
Alessandro Felaco <alessandro.felaco.91@gmail.com>
2025-10-22 23:05:04 +0200

    dev

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,10 +4,17 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=4.1.0"
     }
+    azuread = {
+      source = "hashicorp/azuread"
+    }
   }
 }
 
 provider "azurerm" {
   subscription_id = var.subscription_id
   features {}
+}
+
+provider "azuread" {
+  tenant_id = data.azurerm_client_config.this.tenant_id
 }
