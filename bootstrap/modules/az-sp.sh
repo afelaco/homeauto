3781f84a8654a58e95095b00f28c64c1b1e73d3b
Alessandro Felaco <alessandro.felaco.91@gmail.com>
2025-10-23 09:24:14 +0200

    dev

--- bootstrap/modules/az-sp.sh
+++ bootstrap/modules/az-sp.sh
@@ -1,3 +1,6 @@
+# -----------------------------
+# Create Azure Service Principal for Terraform
+# -----------------------------
 echo "  ➡️ Creating Azure Service Principal $AZ_SP_TF_NAME..."
 AZ_SP_TF_CREDENTIALS=$(az ad sp create-for-rbac \
     --name "$AZ_SP_TF_NAME" \
@@ -8,10 +11,19 @@ echo "  ✅  Azure Service Principal $AZ_SP_TF_NAME created!"
 gh secret set AZ_SP_TF_CREDENTIALS --repo "$GH_REPO" --body "$AZ_SP_TF_CREDENTIALS"
 echo "  ✅  Service Principal credentials set in GitHub Actions!"
 
-echo "  ➡️ Assigning Cloud Application Administrator role to Service Principal $AZ_SP_TF_NAME..."
+# -----------------------------
+# Assign Cloud Application Administrator role to Service Principal
+# -----------------------------
 AZ_SP_TF_OBJECT_ID=$(az ad sp list --display-name "$AZ_SP_TF_NAME" --query "[].id" -o tsv)
-az role assignment create \
-    --assignee "$AZ_SP_TF_OBJECT_ID" \
-    --role "Cloud Application Administrator" \
-    --scope "/subscriptions/$AZ_SUBSCRIPTION_ID"
-echo "  ✅  'Cloud Application Administrator' role assigned to Service Principal $AZ_SP_TF_NAME!"
+ROLE_NAME="Cloud Application Administrator"
+echo "  ➡️ Assigning $ROLE_NAME role to Service Principal $AZ_SP_TF_NAME..."
+ROLE_TEMPLATE_ID=$(az rest \
+    --method GET \
+    --url "https://graph.microsoft.com/v1.0/directoryRoles" \
+    --query "value[?displayName=='$ROLE_NAME'].id" \
+    -o tsv)
+az rest \
+    --method POST \
+    --url "https://graph.microsoft.com/v1.0/directoryRoles/$ROLE_TEMPLATE_ID/members/\$ref" \
+    --body "{\"@odata.id\": \"https://graph.microsoft.com/v1.0/directoryObjects/$AZ_SP_TF_OBJECT_ID\"}"
+echo "  ✅ Successfully assigned $ROLE_NAME role to Service Principal $AZ_SP_TF_NAME"
