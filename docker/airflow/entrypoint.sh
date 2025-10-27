fa49afb9e059ff65f9e6353ab53ecbae40f26fab
Alessandro Felaco <113336615+afelaco@users.noreply.github.com>
2025-10-27 22:02:40 +0100

    Patch Airflow entrypoint.sh (#55)

--- docker/airflow/entrypoint.sh
+++ docker/airflow/entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 # Install wheel from GitLab
 WHEEL_NAME="homeauto-0.1.0-py3-none-any.whl"
 PYPI_URL="https://gitlab.com/api/v4/projects/75607343/packages/pypi/simple"
-GITLAB_TOKEN="${GITLAB_TOKEN}"
+#GITLAB_TOKEN="${GITLAB_TOKEN}"
 
 echo "Installing wheel from GitLab..."
-pip install --index-url "$PYPI_URL" "$WHEEL_NAME"
+#pip install --index-url "$PYPI_URL" "$WHEEL_NAME"
 echo "Wheel installed successfully."
