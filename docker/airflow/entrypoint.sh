c8c397ed33e7d40296c29c864adf54f83ff01da2
Alessandro Felaco <113336615+afelaco@users.noreply.github.com>
2025-10-28 12:17:51 +0100

    Fix entrypoint.sh (#64)

--- docker/airflow/entrypoint.sh
+++ docker/airflow/entrypoint.sh
@@ -25,14 +25,11 @@ fi
 # Start Airflow webserver or scheduler based on the command
 exec airflow "$@"
 
-##!/usr/bin/env bash
-#set -e
-#
-## Install wheel from GitLab
+# Install wheel from GitLab
 #WHEEL_NAME="homeauto-0.1.0-py3-none-any.whl"
 #PYPI_URL="https://gitlab.com/api/v4/projects/75607343/packages/pypi/simple"
 #GITLAB_TOKEN="${GITLAB_TOKEN}"
-#
+
 #echo "Installing wheel from GitLab..."
 #pip install --index-url "$PYPI_URL" "$WHEEL_NAME"
 #echo "Wheel installed successfully."
