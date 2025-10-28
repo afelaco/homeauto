c0acd61fe71b8d09c45beddb87ace211fbd358fd
Alessandro Felaco <113336615+afelaco@users.noreply.github.com>
2025-10-28 11:25:03 +0100

    Build Custom Airflow Image (#58)

--- docker/airflow/entrypoint.sh
+++ docker/airflow/entrypoint.sh
@@ -1,11 +1,38 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -e
 
-# Install wheel from GitLab
-WHEEL_NAME="homeauto-0.1.0-py3-none-any.whl"
-PYPI_URL="https://gitlab.com/api/v4/projects/75607343/packages/pypi/simple"
-GITLAB_TOKEN="${GITLAB_TOKEN}"
+# Change ownership of AIRFLOW_HOME to the RPI admin user
+chown -R "${AIRFLOW_UID}:0" "${AIRFLOW_HOME}"
 
-echo "Installing wheel from GitLab..."
-pip install --index-url "$PYPI_URL" "$WHEEL_NAME"
-echo "Wheel installed successfully."
+# Initialize database
+airflow db check || airflow db init
+
+# Create admin user
+if ! airflow users list | grep -q "${AIRFLOW_USERNAME}"; then
+    echo "Creating Airflow admin user..."
+    airflow users create \
+        --username "${AIRFLOW_USERNAME}" \
+        --password "${AIRFLOW_PASSWORD}" \
+        --firstname "${AIRFLOW_FIRSTNAME}" \
+        --lastname "${AIRFLOW_LASTNAME}" \
+        --email "${AIRFLOW_EMAIL}" \
+        --role "${AIRFLOW_ROLE}"
+
+else
+    echo "Airflow admin user '${AIRFLOW_USERNAME}' already exists."
+fi
+
+# Start Airflow webserver or scheduler based on the command
+exec airflow "$@"
+
+##!/usr/bin/env bash
+#set -e
+#
+## Install wheel from GitLab
+#WHEEL_NAME="homeauto-0.1.0-py3-none-any.whl"
+#PYPI_URL="https://gitlab.com/api/v4/projects/75607343/packages/pypi/simple"
+#GITLAB_TOKEN="${GITLAB_TOKEN}"
+#
+#echo "Installing wheel from GitLab..."
+#pip install --index-url "$PYPI_URL" "$WHEEL_NAME"
+#echo "Wheel installed successfully."
