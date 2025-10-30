1a92d86822dbfe493ae6b8f385ce2fe09c5f4238
Alessandro Felaco <alessandro.felaco.91@gmail.com>
2025-10-30 13:05:03 +0100

    Fix entrypoint.sh

--- docker/airflow/entrypoint.sh
+++ docker/airflow/entrypoint.sh
@@ -5,22 +5,22 @@ set -e
 chown -R "${AIRFLOW_UID}:0" "${AIRFLOW_HOME}"
 
 # Initialize database
-airflow db check || airflow db init
+airflow db init
 
 # Create admin user
-if ! airflow users list | grep -q "${AIRFLOW_USERNAME}"; then
-    echo "Creating Airflow admin user..."
-    airflow users create \
-        --username "${AIRFLOW_USERNAME}" \
-        --password "${AIRFLOW_PASSWORD}" \
-        --firstname "${AIRFLOW_FIRSTNAME}" \
-        --lastname "${AIRFLOW_LASTNAME}" \
-        --email "${AIRFLOW_EMAIL}" \
-        --role "${AIRFLOW_ROLE}"
+#if ! airflow users list | grep -q "${AIRFLOW_USERNAME}"; then
+echo "Creating Airflow admin user..."
+airflow users create \
+    --username "${AIRFLOW_USERNAME}" \
+    --password "${AIRFLOW_PASSWORD}" \
+    --firstname "${AIRFLOW_FIRSTNAME}" \
+    --lastname "${AIRFLOW_LASTNAME}" \
+    --email "${AIRFLOW_EMAIL}" \
+    --role "${AIRFLOW_ROLE}"
 
-else
-    echo "Airflow admin user '${AIRFLOW_USERNAME}' already exists."
-fi
+#else
+#    echo "Airflow admin user '${AIRFLOW_USERNAME}' already exists."
+#fi
 
 # Start Airflow webserver or scheduler based on the command
 exec airflow "$@"
