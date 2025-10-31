8b7ec869232b1b89b85e02c902ab079236f8b5c7
Alessandro Felaco <113336615+afelaco@users.noreply.github.com>
2025-10-31 11:26:30 +0100

    Refactor containers (#89)
    * Refactor containers
    
    * dev
    
    * dev
    
    * dev
    
    * dev

--- docker/airflow/entrypoint.sh
+++ docker/airflow/entrypoint.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 set -e
 
+if [ "$1" = "scheduler" ]; then
+    airflow db migrate
+    airflow users create \
+        --username "${_AIRFLOW_WWW_USER_USERNAME}" \
+        --password "${_AIRFLOW_WWW_USER_PASSWORD}" \
+        --firstname Airflow \
+        --lastname Admin \
+        --role Admin \
+        --email admin@example.com
+fi
+
 exec airflow "$@"
