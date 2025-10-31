9e46cfbce148591c1eec79802adce1e79dc0db41
Alessandro Felaco <alessandro.felaco.91@gmail.com>
2025-10-31 10:51:30 +0100

    Refactor containers

--- docker/airflow/entrypoint.sh
+++ docker/airflow/entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 set -e
 
+airflow db migrate
+airflow users create \
+    --username "${_AIRFLOW_WWW_USER_USERNAME}" \
+    --password "${_AIRFLOW_WWW_USER_PASSWORD}" \
+    --firstname Airflow \
+    --lastname Admin \
+    --role Admin \
+    --email admin@example.com
+
 exec airflow "$@"
