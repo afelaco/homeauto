d2cabffcb2e387b3588f1acd3756b91c8ada8496
Alessandro Felaco <alessandro.felaco.91@gmail.com>
2025-10-31 11:07:25 +0100

    dev

--- docker/airflow/entrypoint.sh
+++ docker/airflow/entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -e
 
-airflow db migrate
-airflow users create \
-    --username "${_AIRFLOW_WWW_USER_USERNAME}" \
-    --password "${_AIRFLOW_WWW_USER_PASSWORD}" \
-    --firstname Airflow \
-    --lastname Admin \
-    --role Admin \
-    --email admin@example.com
+if [ "$1" = "scheduler" ]; then
+    airflow db migrate
+    airflow users create \
+        --username "${_AIRFLOW_WWW_USER_USERNAME}" \
+        --password "${_AIRFLOW_WWW_USER_PASSWORD}" \
+        --firstname Airflow \
+        --lastname Admin \
+        --role Admin \
+        --email admin@example.com
+fi
 
 exec airflow "$@"
