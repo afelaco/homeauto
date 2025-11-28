e05fc7d0786a78cc3411d9399426ab1adaee6995
Alessandro Felaco <alessandro.felaco.91@gmail.com>
2025-11-28 19:48:30 +0100

    Add Scryfall container

--- src/homeauto/load/dimension/dim_collection.py
+++ src/homeauto/load/dimension/dim_collection.py
@@ -1,6 +1,7 @@
 import polars as pl
 
 import homeauto.core.dataset.silver.manabox
+import homeauto.core.dataset.silver.scryfall
 import homeauto.core.table.dimension
 from homeauto.core.table.dimension import DimensionTable
 from homeauto.load import Load
@@ -15,11 +16,58 @@ class LoadDimCollection(Load):
         self.table.replace_table(df=self.get_data())
 
     def get_data(self) -> pl.DataFrame:
-        return self.get_collection().sort("name").select(self.table.schema.columns.keys())
+        cards = self.get_cards()
+        collection = self.get_collection()
+        return (
+            cards.join(
+                other=collection,
+                on="scryfall_id",
+                how="left",
+            )
+            .with_columns(pl.col("collected").fill_null(False))
+            .select(self.table.schema.columns.keys())
+        )
+
+    @staticmethod
+    def get_cards() -> pl.DataFrame:
+        return homeauto.core.dataset.silver.scryfall.cards.read_parquet().select(
+            "scryfall_id",
+            "name",
+            "set",
+            "set_name",
+            "collector_number",
+            "released_at",
+            "layout",
+            "cmc",
+            "type_line",
+            "oracle_text",
+            "power",
+            "toughness",
+            "reserved",
+            "game_changer",
+            "oversized",
+            "promo",
+            "reprint",
+            "variation",
+            "rarity",
+            "full_art",
+            "textless",
+            "booster",
+            "price",
+            "price_foil",
+        )
 
     @staticmethod
     def get_collection() -> pl.DataFrame:
-        return homeauto.core.dataset.silver.manabox.collection.read_parquet()
+        return (
+            homeauto.core.dataset.silver.manabox.collection.read_parquet()
+            .with_columns(collected=pl.lit(True))
+            .select(
+                "scryfall_id",
+                "collected",
+                "foil",
+            )
+        )
 
 
 if __name__ == "__main__":
